f72da964aa44f768d170b4daa4356dccb549a2f7
Zemtsov Renat <zemtsov20@mail.ru>
2022-02-26 00:27:13 +0300

    project fix-2

--- parser-module/src/main/java/emailparser/EmailParserApplication.java
+++ parser-module/src/main/java/emailparser/EmailParserApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication(scanBasePackages={"common.repository"})
 public class EmailParserApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmailParserApplication.class, args);
